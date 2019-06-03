8b3a2530c8461cca936fbb91dc473d68a33d71aa
Azikar <40801104+Azikar@users.noreply.github.com>
2019-06-03 08:17:07 +0300

    changes to userfactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'nickname' => explode(' ',$faker->unique()->name)[0],
         'lvl'=>10,
-        'current_xp'=>20,
+        'current_xp'=>200,
         'possition'=>'student',
         'profile_picture'=>'testpic.png',
         'about'=>'something about me'
